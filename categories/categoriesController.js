2747735d125d09fade69c8b0bf4887c873c6347d
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-11-03 22:11:01 -0300

    ed edit
    terminado a parte de editar

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -6,6 +6,7 @@ const slugfy =  require("slugify");
 router.get("/categories", (req, res)=>{
     res.send('rota de categgorias');
 });
+
 router.get("/admin/articles/new",(req, res)=>{
     res.render('../views/Admin/categories/new');
 
@@ -13,11 +14,7 @@ router.get("/admin/articles/new",(req, res)=>{
 router.post("/categories/save", (req, res)=>{
    var titla = req.body.title;
 
-   if(titla == undefined){
-
-    res.redirect('/admin/articles/new')
-   
-   }else{
+   if( isNaN( titla) && titla!= undefined ){
 
     category.create({
         title:titla,
@@ -25,17 +22,21 @@ router.post("/categories/save", (req, res)=>{
 
     }).then(()=>{ res.redirect("/admin/categories")})
 
+    
+   }else{
+
+    //res.redirect('/admin/articles/new')
+    res.send('ta mandando errado ai');
+
    }
 });
 
 router.get("/admin/categories",(req, res)=>{
 
-    category.findAll().then(categories =>{
+    category.findAll({raw: true,order: [['id', 'DESC']]}).then(categories =>{
 
         res.render("./Admin/categories/index", {categoriesFront: categories} );
     })
-
-    
 
 });
 
@@ -48,9 +49,7 @@ router.post('/categories/delete', (req, res)=>{
 
                     id:id
                 }
-            }).then(()=>{
-                res.redirect('/admin/categories')
-            })
+            }).then(()=>{ res.redirect('/admin/categories')})
 
       
 
@@ -61,6 +60,33 @@ router.post('/categories/delete', (req, res)=>{
     }
 })
 
+router.get("/admin/categories/edit/:id", (req , res)=>{
+    var id = req.params.id;
+    if(isNaN(id)){res.redirect('/admin/categories')}
+    category.findByPk(id).then(categoria=>{
+        if (categoria != undefined){
+
+            res.render("./Admin/categories/edit", {category:categoria})
+
+
+        }else{ 
+            res.redirect('/admin/categories')
+        }
+    })
+});
+
+router.post("/categories/update", (req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+
+    category.update({title:title, slugfy:slugfy(title)},{
+        where:{
+            id:id
+        }
+    }).then(()=>{ res.redirect('/admin/categories')})
+
+
+});
 
 
 module.exports = router;
